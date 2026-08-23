6ead6f70be1ab478c354748ea550141c25e177a9
Alinafe Matenda <alinafe.matenda@gmail.com>
2026-08-23 07:03:06 -0700

    Protect foundation tag invariants (#9)
    * Protect foundation tag invariants
    
    * Format foundation tag merge

--- variables.tf
+++ variables.tf
@@ -27,3 +27,23 @@ variable "env" {
     error_message = "env must be one of dev, stage, or prod."
   }
 }
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+
+  validation {
+    condition = alltrue([
+      for key, value in var.tags : length(trimspace(key)) > 0 && length(trimspace(value)) > 0
+    ])
+    error_message = "tag keys and values must not be blank."
+  }
+
+  validation {
+    condition = length(setintersection(
+      toset(keys(var.tags)),
+      toset(["project", "environment", "managed_by"])
+    )) == 0
+    error_message = "tags must not override project, environment, or managed_by."
+  }
+}
